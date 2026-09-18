72cae7833eb0288e65150c23d34bba9047736600
wylfred <wylfredtapouamo@gmail.com>
2026-09-18 13:09:13 +0100

    modification de SecurityConfig en ajoutant authenticationManager()

--- src/main/java/com/wyltech/taskmanager/config/SecurityConfig.java
+++ src/main/java/com/wyltech/taskmanager/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.wyltech.taskmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -35,6 +37,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
